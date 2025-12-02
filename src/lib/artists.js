f539de12f43b23aaca5fc08ad7ebb1f6ed4c0f76
mike <miguel.cabrera.landin@gmail.com>
2025-12-02 12:56:25 +0100

    FEAT: getYourTopArtist

--- src/lib/artists.js
+++ src/lib/artists.js
@@ -1,29 +1,43 @@
 import { getAccessToken } from "@/lib/auth.js";
 
-export async function getArtistByName(userIn) { // return the artist you search
-    const token = getAccessToken();
+export async function getArtistByName(userIn){ // return the artist you search
+	const token = getAccessToken();
 
-    // obtain the artist
-    const result = await fetch(
-        `https://api.spotify.com/v1/search?q=${userIn}&type=artist&market=ES&limit=1`,
-        {
-            headers: { 'Authorization': `Bearer ${token}` }
-        }
-    );
-    const artist = await result.json();
-    return artist
+	// obtain the artist
+	const result = await fetch(
+		`https://api.spotify.com/v1/search?q=${userIn}&type=artist&market=ES&limit=1`,
+		{
+			headers: { 'Authorization': `Bearer ${token}` }
+		}
+	);
+	const artist = await result.json();
+	return artist
 }
 
-export async function getArtistById(artistId) { // return the artist searching by id
-    const token = getAccessToken();
+export async function getArtistById(id){
+	const token = getAccessToken();
 
-    //obtain the artist by id
-    const result = await fetch(
-        `https://api.spotify.com/v1/artist/${artistId}`,
-        {
-            headers: { 'Authorization': `Bearer ${token}` }
-        }
-    );
-    const artist = await result.json();
-    return artist
+	//obtain the artist
+	const result = await fetch(
+		`https://api.spotify.com/v1/artists/${id}`,
+		{
+			headers: { 'Authorization': `Bearer ${token}` }
+		}
+	);
+	const artist = await result.json();
+	return artist;
+}
+
+export async function getYourTopArtists(){ // get your top 5 most listened artist
+	const token = getAccessToken();
+
+	// obtain the artist
+	const result = await fetch(
+		`https://api.spotify.com/v1/me/top/artists?limit=5`,
+		{
+			headers: { 'Authorization': `Bearer ${token}` }
+		}
+	);
+	const artists = await result.json();
+	return artists
 }
