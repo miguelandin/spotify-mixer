3c8ead009627587be33e2662ecc3764618878da5
miguelandin <miguel.cabrera.landin@gmail.com>
2025-12-05 17:00:08 +0100

    GENRES

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,9 @@
 'use client';
 
 import TrackImg from "../components/TrackImg";
+import { getGenreByName, getGenreByIndex, getAllGenres } from "@/lib/genres";
 export default function DashBoardPage() {
+    console.log(getGenreByName("heavymetal"))
     return (<>
         <TrackImg id="42a9PusvgFnyH0WgdCn6PV" />
         <TrackImg id="11dFghVXANMlKmJXsNCbNl" />
